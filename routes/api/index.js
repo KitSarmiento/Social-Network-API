6dea1aa90c02a09a76db370385d541a4fab31016
KitSarmiento <ksarmiento020@gmail.com>
2023-11-12 13:51:02 -0500

    Added a thought routes.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,4 @@
+// Used Module 18 activity 23 as a reference and update the code accordingly based on the acceptance Criteria.
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const thoughtRoutes = require("./thoughtRoutes");
